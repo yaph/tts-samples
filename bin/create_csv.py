6e5668928e0f8229d1346913bf3969738befca44
Ramiro Gómez <code@ramiro.org>
2025-02-10 22:43:13 +0100

    Add Country column

--- bin/create_csv.py
+++ bin/create_csv.py
@@ -3,9 +3,11 @@ from pathlib import Path
 import csv
 import json
 
+from pycountry import countries
+
 voices = json.loads(Path('data/voices.json').read_text())
 voice_names = {}
-rows = [['Name', 'Gender', 'Language', 'Categories', 'Personalities', 'Speech Sample']]
+rows = [['Name', 'Gender', 'Language', 'Country', 'Categories', 'Personalities', 'Speech Sample']]
 
 for sample in Path('mp3').glob('**/*.mp3'):
     lang, voice = sample.parent.name, sample.stem
@@ -14,9 +16,11 @@ for sample in Path('mp3').glob('**/*.mp3'):
 for voice in voices:
     name = voice['ShortName']
     if name in voice_names:
+        country = countries.get(alpha_2=voice['Locale'].split('-')[1]).name
         rows.append([voice['FriendlyName'].split(' - ')[0].replace('Microsoft ', '').replace(' Online (Natural)', ''),
                      voice['Gender'],
                      voice_names[name]['language'],
+                     country,
                      ', '.join(voice['VoiceTag']['ContentCategories']),
                      ', '.join(voice['VoiceTag']['VoicePersonalities']),
                      voice_names[name]['path']
